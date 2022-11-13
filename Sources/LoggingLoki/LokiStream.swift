928e3c6547fa6df8e6eb83d36638095d0bd11825
Timo <38291523+lovetodream@users.noreply.github.com>
2022-11-13 14:14:07 +0100

    fix: prevent scientific notation (second try)

--- Sources/LoggingLoki/LokiStream.swift
+++ Sources/LoggingLoki/LokiStream.swift
@@ -1,12 +1,20 @@
+import class Foundation.NumberFormatter
+import class Foundation.NSNumber
+
 struct LokiStream: Encodable {
     var stream: Dictionary<String, String>
     var values: Array<Array<String>>
 
     init(_ logs: [LokiLog], with labels: LokiLabels) {
         self.stream = labels
-        self.values = logs.map{ log in
-            let timestamp = Int64(log.timestamp.timeIntervalSince1970 * 1_000_000_000)
-            return [String(format: "%.0f", timestamp), log.message]
+        self.values = logs.compactMap { log in
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            let timestamp = Int64(log.timestamp.timeIntervalSince1970 * 1_000_000_000) as NSNumber
+            guard let formattedTimestamp = formatter.string(from: timestamp) else {
+                return nil
+            }
+            return [formattedTimestamp, log.message]
         }
     }
 }
