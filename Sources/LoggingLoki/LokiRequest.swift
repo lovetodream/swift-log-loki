54324cac2251d558f9da710d42057c41736dd910
Timo <38291523+lovetodream@users.noreply.github.com>
2024-04-22 14:49:14 +0200

    feat: add license and soundness checks

--- Sources/LoggingLoki/LokiRequest.swift
+++ Sources/LoggingLoki/LokiRequest.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftLogLoki open source project
+//
+// Copyright (c) 2024 Timo Zacherl and the SwiftLogLoki project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 struct LokiRequest: Encodable, Sendable {
     var streams: [LokiStream]
 
--- Tests/LoggingLokiTests/MockClock.swift
+++ Tests/LoggingLokiTests/MockClock.swift
@@ -1,5 +1,18 @@
 //===----------------------------------------------------------------------===//
 //
+// This source file is part of the SwiftLogLoki open source project
+//
+// Copyright (c) 2024 Timo Zacherl and the SwiftLogLoki project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+//===----------------------------------------------------------------------===//
+//
 // This source file is part of the Swift OTel open source project
 //
 // Copyright (c) 2023 Moritz Lang and the Swift OTel project authors
